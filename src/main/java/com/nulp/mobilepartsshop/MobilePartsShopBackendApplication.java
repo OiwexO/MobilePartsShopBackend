b6adca500270626c03a69d5fe8c7beadadab3f4a
OiwexO <denyskostetskyi@gmail.com>
2024-01-28 22:28:58 +0200

    Initial commit

---
+++ src/main/java/com/nulp/mobilepartsshop/MobilePartsShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nulp.mobilepartsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilePartsShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobilePartsShopBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nulp/mobilepartsshop/MobilePartsShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nulp.mobilepartsshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilePartsShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
