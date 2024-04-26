2a019cd2c867564caffdbb03db6553090e9e6b94
OiwexO <denyskostetskyi@gmail.com>
2024-04-26 19:37:58 +0300

    address + device, refactoring

--- src/main/java/com/nulp/mobilepartsshop/api/v1/adminPanel/service/UserRegistrationService.java
+++ src/main/java/com/nulp/mobilepartsshop/api/v1/adminPanel/service/UserRegistrationService.java
@@ -2,7 +2,6 @@ package com.nulp.mobilepartsshop.api.v1.adminPanel.service;
 
 import com.nulp.mobilepartsshop.api.v1.adminPanel.dto.request.UserRegistrationRequest;
 import com.nulp.mobilepartsshop.api.v1.adminPanel.dto.response.UserRegistrationResponse;
-import com.nulp.mobilepartsshop.core.enums.UserAuthority;
 import com.nulp.mobilepartsshop.exception.adminPanel.AdminAlreadyExistsException;
 import com.nulp.mobilepartsshop.exception.authentication.UsernameAlreadyUsedException;
 
