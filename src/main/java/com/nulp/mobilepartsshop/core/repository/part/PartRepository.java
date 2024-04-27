570d773886c767f3ddb7b7173c34c182ce45ded3
OiwexO <denyskostetskyi@gmail.com>
2024-04-27 19:09:21 +0300

    + part recommendations

--- src/main/java/com/nulp/mobilepartsshop/core/repository/part/PartRepository.java
+++ src/main/java/com/nulp/mobilepartsshop/core/repository/part/PartRepository.java
@@ -3,8 +3,6 @@ package com.nulp.mobilepartsshop.core.repository.part;
 import com.nulp.mobilepartsshop.core.entity.part.Part;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface PartRepository extends JpaRepository<Part, Long> {
 
     boolean existsByNameAndManufacturerIdAndDeviceTypeIdAndPartTypeId(
