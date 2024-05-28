278fbb590afdbba3e1f954d3cb91c4a6d8ec4fab
OiwexO <denyskostetskyi@gmail.com>
2024-05-28 16:19:36 +0300

    RSA decryption

--- src/main/java/com/nulp/mobilepartsshop/api/v1/authentication/dto/response/EncryptionDataResponse.java
+++ src/main/java/com/nulp/mobilepartsshop/api/v1/authentication/dto/response/EncryptionDataResponse.java
@@ -1,9 +1,6 @@
 package com.nulp.mobilepartsshop.api.v1.authentication.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Builder
 public record EncryptionDataResponse(String cipher, String keyAlgorithm, String publicKeyBase64) {
--- src/main/java/com/nulp/mobilepartsshop/core/entity/authentication/RegistrationData.java
+++ src/main/java/com/nulp/mobilepartsshop/core/entity/authentication/RegistrationData.java
@@ -1,6 +1,5 @@
 package com.nulp.mobilepartsshop.core.entity.authentication;
 
-import com.nulp.mobilepartsshop.core.entity.user.User;
 import lombok.Builder;
 
 @Builder
